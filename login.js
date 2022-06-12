0b02b4442301eb48ec2f300fc3af8be0a87d5dc8
syedwajith <84230157+syedwajith@users.noreply.github.com>
2022-06-12 15:34:44 +0530

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,19 @@ function Signin()
 {
     var username = document.getElementById("uname").value;
     var password = document.getElementById("pwd").value;
-    if(username=="syed" && password=="12345")
+    if(username.length==0)
+    {
+        alert("Please enter the username");
+    }
+    else if(password.length==0)
+    {
+        alert("Please enter the password");
+    }
+    else if(password!=password)
+    {
+        alert("Please enter the correct password")
+    }
+    else if(username=="syed" && password=="123")
     {
         window.location.href="home.html";
     }
